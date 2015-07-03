d6cdf759878d4a23a62db28b78d232cc63125803
syougun360 <syougun360@infoseek.jp>
2015-07-03 18:48:27 +0900

    #693 シーンチェンジシステム実装

---
+++ Assets/Scene/IScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IScene : MonoBehaviour {
+
+    [SerializeField]
+    protected FadeTimeData fadeTime = new FadeTimeData(1, 1);
+
+
+}
---
+++ Assets/Title/TitleManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManager : IScene {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 仮
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.Instance.StartChange(SceneNameManager.Scene.CharacterSelect,fadeTime);
+        }
+	}
+}
