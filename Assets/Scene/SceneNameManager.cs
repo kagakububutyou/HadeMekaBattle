4d56e4c58b790c2e84f944a0c7acdb9bc0611990
syougun360 <syougun360@infoseek.jp>
2015-07-03 14:34:58 +0900

    #638 各シーンをUnityで作成

---
+++ Assets/Scene/SceneNameManager.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// シーン名を定数で管理するクラス
+/// </summary>
+public class SceneNameManager
+{
+	public const string Battle = "Battle";
+	public const string CharacterSelect = "CharacterSelect";
+	public const string MapSelect = "MapSelect";
+	public const string Title = "Title";
+	public const string WeaponSelect = "WeaponSelect";
+
+    public enum Scene
+    {
+	    Battle,
+	    CharacterSelect,
+	    MapSelect,
+	    Title,
+	    WeaponSelect,
+    }
+}
