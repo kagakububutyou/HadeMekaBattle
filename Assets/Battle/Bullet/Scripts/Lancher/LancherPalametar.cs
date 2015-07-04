acf52a6d91e70eb05553d2acac18fd392ada67b3
Gai Takakura <gixa.aturakka@gmail.com>
2015-07-04 15:08:26 +0900

    ランチャーの降下
    重力の法則は完全に無視しているが、速度などを調整すれば問題ないかと思います

--- Assets/Battle/Bullet/Scripts/Lancher/LancherPalametar.cs
+++ Assets/Battle/Bullet/Scripts/Lancher/LancherPalametar.cs
@@ -3,5 +3,11 @@ using System.Collections;
 
 // ランチャーのパラメータ
 public class LancherPalametar : BulletBasePalametar {
+	[SerializeField]
+	private float vectorRotationY = -0.5f;	// ベクトルでの最終的に向いて欲しい向き(Y)
+	[SerializeField]
+	private float rotationSpeed = 15.0f;	// 一秒間に曲がる角度
 
+	public float GetVectorY{get{return vectorRotationY;}}
+	public float GetRotationSpeed{get{return rotationSpeed;}}
 }
