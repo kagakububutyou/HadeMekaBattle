2006022fdc29cd636b2df532e8e08edf715bc4f7
Gai Takakura <g.takakura@outlook.com>
2015-07-07 20:09:34 +0900

    ブランチの再構築

---
+++ Assets/Battle/Bullet/Scripts/LifeDroper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 弾丸の寿命設定
+public class LifeDroper : MonoBehaviour {
+	[SerializeField]
+	private float lifeTime = 5.0f;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime -= 1.0f * Time.deltaTime;
+		if(lifeTime <= 0.0f) Destroy(this.gameObject);
+	}
+}
