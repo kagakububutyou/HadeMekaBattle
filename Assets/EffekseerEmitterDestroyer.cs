7a656bc572840d83c45310d062a16a94950f673f
syougun360 <syougun360@infoseek.jp>
2015-07-08 14:32:05 +0900

    エフェクトの自動削除、生成のメソッド実装

---
+++ Assets/EffekseerEmitterDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffekseerEmitterDestroyer : MonoBehaviour {
+
+    EffekseerEmitter effect = null;
+
+	// Use this for initialization
+	void Start () {
+        effect = GetComponent<EffekseerEmitter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!effect.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
