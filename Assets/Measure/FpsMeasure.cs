14dddd663de4557d015ac5016cc9f8ae9c7ac459
syougun360 <syougun360@infoseek.jp>
2015-07-18 16:41:31 +0900

    FPSとRRTを描画実装

---
+++ Assets/Measure/FpsMeasure.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FpsMeasure : MonoBehaviour {
+
+    int frameCount;
+    float nextTime;
+
+    Text text = null;
+
+	// Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        nextTime = Time.time + 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        frameCount++;
+
+        if (Time.time >= nextTime)
+        {
+            text.text = "FPS : " + frameCount;
+            frameCount = 0;
+            nextTime += 1;
+        }
+	}
+}
